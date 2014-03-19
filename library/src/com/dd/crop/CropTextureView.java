2002d2125aa42b884c05d49b8bb8480a7ffd7677
Dmytro Danylyk <dmytrodanylyk@gmail.com>
2014-03-19 23:26:27 +0200

    Initial commit

---
+++ library/src/com/dd/crop/CropTextureView.java
@@ -0,0 +1,234 @@
+package com.dd.crop;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Matrix;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CropTextureView extends TextureView implements TextureView.SurfaceTextureListener {
+
+    private boolean mIsPlayCalled;
+
+    public enum ScaleType {
+        CENTER_CROP, START, END;
+    }
+
+    // Log tag
+    private static final String TAG = CropTextureView.class.getName();
+
+    private MediaPlayer mMediaPlayer;
+    private SurfaceTexture mSurfaceTexture;
+
+    private float mVideoHeight;
+    private float mVideoWidth;
+
+    private boolean mIsDataSourceSet;
+    private boolean mIsViewAvailable;
+
+    private ScaleType mScaleType;
+
+    public CropTextureView(Context context) {
+        super(context);
+        initView();
+    }
+
+    public CropTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    public CropTextureView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initView();
+    }
+
+    private void initView() {
+        mMediaPlayer = new MediaPlayer();
+        setScaleType(ScaleType.CENTER_CROP);
+        setSurfaceTextureListener(this);
+    }
+
+    public void setScaleType(ScaleType scaleType) {
+        mScaleType = scaleType;
+    }
+
+    private void updateTextureViewSize() {
+        float viewWidth = getWidth();
+        float viewHeight = getHeight();
+
+        float scaleX = 1.0f;
+        float scaleY = 1.0f;
+
+        if (mVideoWidth > viewWidth && mVideoHeight > viewHeight) {
+            scaleX = mVideoWidth / viewWidth;
+            scaleY = mVideoHeight / viewHeight;
+        } else if (mVideoWidth < viewWidth && mVideoHeight < viewHeight) {
+            scaleY = viewWidth / mVideoWidth;
+            scaleX = viewHeight / mVideoHeight;
+        } else if (viewWidth > mVideoWidth) {
+            scaleY = (viewWidth / mVideoWidth) / (viewHeight / mVideoHeight);
+        } else if (viewHeight > mVideoHeight) {
+            scaleX = (viewHeight / mVideoHeight) / (viewWidth / mVideoWidth);
+        }
+
+        // Calculate pivot points, in our case crop from center
+        int pivotPointX;
+        int pivotPointY;
+
+        switch (mScaleType) {
+            case START:
+                pivotPointX = 0;
+                pivotPointY = 0;
+                break;
+            case END:
+                pivotPointX = (int) (viewWidth);
+                pivotPointY = (int) (viewHeight);
+                break;
+            case CENTER_CROP:
+                pivotPointX = (int) (viewWidth / 2);
+                pivotPointY = (int) (viewHeight / 2);
+                break;
+            default:
+                pivotPointX = (int) (viewWidth / 2);
+                pivotPointY = (int) (viewHeight / 2);
+                break;
+        }
+
+        Matrix matrix = new Matrix();
+        matrix.setScale(scaleX, scaleY, pivotPointX, pivotPointY);
+
+        setTransform(matrix);
+    }
+
+    public void setUrlDataSource(String url) {
+        try {
+            mMediaPlayer.setDataSource(getContext(), Uri.parse(url));
+            mIsDataSourceSet = true;
+        } catch (IOException e) {
+            Log.d(TAG, e.getMessage());
+        }
+    }
+
+    public void setAssetDataSource(File filePath) {
+        try {
+            AssetFileDescriptor afd = getContext().getAssets().openFd(filePath.getPath());
+            long startOffset = afd.getStartOffset();
+            long length = afd.getLength();
+            mMediaPlayer.setDataSource(afd.getFileDescriptor(), startOffset, length);
+            mIsDataSourceSet = true;
+        } catch (IOException e) {
+            Log.d(TAG, e.getMessage());
+        }
+    }
+
+    public void play() {
+        if (mMediaPlayer.isPlaying()) {
+            Log.w(TAG, "play() was called but video is already playin.");
+            return;
+        }
+
+        mIsPlayCalled = true;
+        if (!mIsDataSourceSet) {
+            Log.w(TAG, "play() was called but data source was not set.");
+            return;
+        }
+
+        if (!mIsViewAvailable) {
+            Log.d(TAG, "play() was called but view is not available yet, waiting.");
+            return;
+        }
+
+        Surface surface = new Surface(mSurfaceTexture);
+        try {
+
+            mMediaPlayer.setSurface(surface);
+            mMediaPlayer.setLooping(true);
+
+            mMediaPlayer.setOnVideoSizeChangedListener(
+                    new MediaPlayer.OnVideoSizeChangedListener() {
+                        @Override
+                        public void onVideoSizeChanged(MediaPlayer mp, int width, int height) {
+                            mVideoWidth = width;
+                            mVideoHeight = height;
+                            updateTextureViewSize();
+                        }
+                    }
+            );
+
+            // don't forget to call MediaPlayer.prepareAsync() method when you use constructor for
+            // creating MediaPlayer
+            mMediaPlayer.prepareAsync();
+
+            // Play video when the media source is ready for playback.
+            mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mediaPlayer.start();
+                }
+            });
+
+        } catch (IllegalArgumentException e) {
+            Log.d(TAG, e.getMessage());
+        } catch (SecurityException e) {
+            Log.d(TAG, e.getMessage());
+        } catch (IllegalStateException e) {
+            Log.d(TAG, e.toString());
+        }
+    }
+
+    public void pause() {
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.pause();
+        }
+    }
+
+    public void stop() {
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.stop();
+        }
+    }
+
+    public void resume() {
+        if (!mMediaPlayer.isPlaying()) {
+            mMediaPlayer.start();
+        }
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mMediaPlayer;
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+        mSurfaceTexture = surfaceTexture;
+        mIsViewAvailable = true;
+        if (mIsDataSourceSet && mIsPlayCalled) {
+            Log.d(TAG, "View is available and play() was called.");
+            play();
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+}
---
+++ sample/src/com/dd/sample/crop/SampleActivity.java
@@ -0,0 +1,72 @@
+package com.dd.sample.crop;
+
+import com.dd.crop.CropTextureView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SampleActivity extends Activity implements View.OnClickListener {
+
+    // Video file url
+    private static final String FILE_URL = "http://www.w3schools.com/html/mov_bbb.mp4";
+    private CropTextureView mCropTextureView1;
+    private CropTextureView mCropTextureView2;
+    private CropTextureView mCropTextureView3;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        initView();
+    }
+
+    private void initView() {
+        mCropTextureView1 = (CropTextureView) findViewById(R.id.cropTextureView1);
+        mCropTextureView1.setScaleType(CropTextureView.ScaleType.START);
+        mCropTextureView1.setUrlDataSource(FILE_URL);
+        mCropTextureView1.play();
+
+        mCropTextureView2 = (CropTextureView) findViewById(R.id.cropTextureView2);
+        mCropTextureView2.setScaleType(CropTextureView.ScaleType.CENTER_CROP);
+        mCropTextureView2.setUrlDataSource(FILE_URL);
+        mCropTextureView2.play();
+
+        mCropTextureView3 = (CropTextureView) findViewById(R.id.cropTextureView3);
+        mCropTextureView3.setScaleType(CropTextureView.ScaleType.END);
+        mCropTextureView3.setUrlDataSource(FILE_URL);
+        mCropTextureView3.play();
+
+        findViewById(R.id.btnPlay).setOnClickListener(this);
+        findViewById(R.id.btnPause).setOnClickListener(this);
+        findViewById(R.id.btnStop).setOnClickListener(this);
+        findViewById(R.id.btnResume).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnPlay:
+                mCropTextureView1.play();
+                mCropTextureView2.play();
+                mCropTextureView3.play();
+                break;
+            case R.id.btnPause:
+                mCropTextureView1.pause();
+                mCropTextureView2.pause();
+                mCropTextureView3.pause();
+                break;
+            case R.id.btnStop:
+                mCropTextureView1.stop();
+                mCropTextureView2.stop();
+                mCropTextureView3.stop();
+                break;
+            case R.id.btnResume:
+                mCropTextureView1.resume();
+                mCropTextureView2.resume();
+                mCropTextureView3.resume();
+                break;
+        }
+    }
+}
